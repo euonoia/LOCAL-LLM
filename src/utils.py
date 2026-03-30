2f941fc9fcc1e0914eb570a5d36abf73864b031a
Ken Española <revas4192@gmail.com>
2026-03-30 10:01:25 +0800

    initial commit

--- src/utils.py
+++
@@ -1,9 +0,0 @@
-import os
-from .config import DOC_FOLDER, GREETING_FILE
-
-def get_custom_greeting():
-    path = os.path.join(DOC_FOLDER, GREETING_FILE)
-    if os.path.exists(path):
-        with open(path, "r", encoding="utf-8") as f:
-            return f.read().strip()
-    return "Euonoia: System online. How can I help you today?"
